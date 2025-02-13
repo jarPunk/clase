19e35469d241d65ae3ec353eea96e318928e5c5d
jarPunk <jar_ax@hotmail.com>
2025-02-12 21:46:59 -0400

    multiplicador correcciones

---
+++ src/multiplicador.test.js
@@ -0,0 +1,9 @@
+import multiplicar from "./multiplicador.js";
+
+describe("Multiplicar", () => {
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(3, 2)).toEqual(5);
+  });
+});
+
+
